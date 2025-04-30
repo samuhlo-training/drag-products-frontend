e7c39ade3ef1d7493a25e44f5ff1fc1602bfa79c
samuhlo <samu13lop@gmail.com>
2025-04-30 14:28:37 +0200

    + deploy fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import CategoryEditorView from "./features/category-editor/CategoryEditorView";
 import PreviewPage from "./features/category-editor/PreviewPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import logoUrl from "/src/assets/logo_not.svg";
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
         <header className="bg-white p-4 flex flex-col items-center justify-center gap-4">
-          <img
-            src="/assets/logo_not.svg"
-            alt="Not AZara Logo"
-            className="w-64"
-          />
+          <img src={logoUrl} alt="Not A Zara Logo" className="w-64" />
         </header>
         <Routes>
           <Route path="/" element={<CategoryEditorView />} />
