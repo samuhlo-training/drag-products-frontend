38eba469f25633b16ffddf47df4a9f51d492e573
samuhlo <samu13lop@gmail.com>
2025-04-30 14:32:44 +0200

    fix preview filas vacias

--- src/features/category-editor/PreviewPage.tsx
+++ src/features/category-editor/PreviewPage.tsx
@@ -33,48 +33,54 @@ const PreviewPage: React.FC = () => {
             No hay filas ni productos para mostrar.
           </p>
         ) : (
-          rows.map((row) => (
-            <section
-              key={row.id}
-              className={`mb-6 pb-8 ${
-                alignmentMap[row.template || "left"]
-              } flex flex-col `}
-            >
-              <div
-                className={`flex gap-8 ${
+          //Comprobar que no este vacia
+          rows
+            .filter((row) => row.products.length > 0)
+            .map((row) => (
+              <section
+                key={row.id}
+                className={`mb-6 pb-8 ${
                   alignmentMap[row.template || "left"]
-                } flex-wrap`}
+                } flex flex-col `}
               >
-                {row.products.length === 0 ? (
-                  <div className="text-gray-300 italic py-8">Fila vacía</div>
-                ) : (
-                  row.products.map((product) => (
-                    <div
-                      key={product.id}
-                      className="bg-white overflow-hidden w-80 h-auto flex flex-col items-center "
-                    >
-                      <img
-                        src={product.imageUrl || "/src/assets/placeholder.jpg"}
-                        alt={product.name}
-                        className="w-full h-[55vh] object-cover"
-                        onError={(e) =>
-                          (e.currentTarget.src = "/src/assets/placeholder.jpg")
-                        }
-                      />
-                      <div className="pt-2 w-full flex flex-col items-start">
-                        <span className="text-[11px] font-medium uppercase truncate w-full text-left">
-                          {product.name}
-                        </span>
-                        <span className="text-[11px] text-gray-600 w-full text-left">
-                          {product.price} EUR
-                        </span>
+                <div
+                  className={`flex gap-8 ${
+                    alignmentMap[row.template || "left"]
+                  } flex-wrap`}
+                >
+                  {row.products.length === 0 ? (
+                    <div className="text-gray-300 italic py-8">Fila vacía</div>
+                  ) : (
+                    row.products.map((product) => (
+                      <div
+                        key={product.id}
+                        className="bg-white overflow-hidden w-80 h-auto flex flex-col items-center "
+                      >
+                        <img
+                          src={
+                            product.imageUrl || "/src/assets/placeholder.jpg"
+                          }
+                          alt={product.name}
+                          className="w-full h-[55vh] object-cover"
+                          onError={(e) =>
+                            (e.currentTarget.src =
+                              "/src/assets/placeholder.jpg")
+                          }
+                        />
+                        <div className="pt-2 w-full flex flex-col items-start">
+                          <span className="text-[11px] font-medium uppercase truncate w-full text-left">
+                            {product.name}
+                          </span>
+                          <span className="text-[11px] text-gray-600 w-full text-left">
+                            {product.price} EUR
+                          </span>
+                        </div>
                       </div>
-                    </div>
-                  ))
-                )}
-              </div>
-            </section>
-          ))
+                    ))
+                  )}
+                </div>
+              </section>
+            ))
         )}
       </main>
     </div>
